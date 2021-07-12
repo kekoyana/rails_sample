2d61cc440d26faf5b4468c1883058cea7e20c214
kekoyana <kekoyana0@gmail.com>
2021-07-13 01:19:48 +0900

    ファイルアップロードのテストはメモリに保存する

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,11 +2,19 @@
 
 require 'shrine'
 require 'shrine/storage/file_system'
+require "shrine/storage/memory"
 
-Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
-  store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads'),       # permanent
-}
+if Rails.env.test?
+  Shrine.storages = {
+    cache: Shrine::Storage::Memory.new,
+    store: Shrine::Storage::Memory.new,
+  }
+else
+  Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
+    store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads'),       # permanent
+  }
+end
 
 Shrine.plugin :derivatives, create_on_promote: true
 Shrine.plugin :activerecord           # loads Active Record integration
