8b71078ea5376fbedb0f6893f363beecf505df2c
kekoyana <kekoyana0@gmail.com>
2021-07-13 01:04:09 +0900

    サムネイルの作成

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,5 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:article) { build(:article) }
+
+  it 'fileのサムネイル作成' do
+    article.file = File.open('spec/fixtures/200x200.png')
+    article.save!
+    expect(article.file_url(:small)).not_to be_nil
+  end
 end
