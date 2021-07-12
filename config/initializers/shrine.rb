126dcf1abe879d956d44eb069c915d40d3b0fca1
kekoyana <kekoyana0@gmail.com>
2021-07-13 01:32:09 +0900

    install libvips to circleci

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,19 +2,19 @@
 
 require 'shrine'
 require 'shrine/storage/file_system'
-require "shrine/storage/memory"
+require 'shrine/storage/memory'
 
-if Rails.env.test?
-  Shrine.storages = {
-    cache: Shrine::Storage::Memory.new,
-    store: Shrine::Storage::Memory.new,
-  }
-else
-  Shrine.storages = {
-    cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
-    store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads'),       # permanent
-  }
-end
+Shrine.storages = if Rails.env.test?
+                    {
+                      cache: Shrine::Storage::Memory.new,
+                      store: Shrine::Storage::Memory.new,
+                    }
+                  else
+                    {
+                      cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'), # temporary
+                      store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads'),       # permanent
+                    }
+                  end
 
 Shrine.plugin :derivatives, create_on_promote: true
 Shrine.plugin :activerecord           # loads Active Record integration
